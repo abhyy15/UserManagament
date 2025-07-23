7ae4888a2b64b2d22cd2f51417ff9e3d54e0aad0
ABHAY <abhay.kumar@mawaimail.com>
2025-07-23 14:17:36 +0530

    removing scehma

--- database/migrations/2025_07_22_153942_create_roles_table.php
+++ database/migrations/2025_07_22_153942_create_roles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('abhay.roles', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique(); 
             $table->timestamps();
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('abhay.roles');
+        Schema::dropIfExists('roles');
     }
 };
